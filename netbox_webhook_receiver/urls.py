8a43856569b922225255478b49d01197f21d341d
Lukasz Polanski <wookasz@gmail.com>
2023-06-26 14:07:20 +0200

    skip formating urls.py by black

--- netbox_webhook_receiver/urls.py
+++ netbox_webhook_receiver/urls.py
@@ -1,4 +1,5 @@
 # ruff: noqa: E501
+# fmt: off
 from netbox.views.generic import ObjectChangeLogView
 from django.urls import path
 from . import models, views
@@ -8,34 +9,12 @@ urlpatterns = (
     # Receive Webhook Messages
     path("webhooks/gitlab/<uuid:random_path>/", gitlab_webhook),
     # Webhook Receiver
-    path(
-        "receivers/",
-        views.WebhookReceiverListView.as_view(),
-        name="webhookreceiver_list",
-    ),
-    path(
-        "receivers/add/",
-        views.WebhookReceiverEditView.as_view(),
-        name="webhookreceiver_add",
-    ),
-    path(
-        "receivers/<int:pk>/",
-        views.WebhookReceiverView.as_view(),
-        name="webhookreceiver",
-    ),
-    path(
-        "receivers/<int:pk>/edit/",
-        views.WebhookReceiverEditView.as_view(),
-        name="webhookreceiver_edit",
-    ),
-    path(
-        "receivers/<int:pk>/delete/",
-        views.WebhookReceiverDeleteView.as_view(),
-        name="webhookreceiver_delete",
-    ),
-    path(
-        "receivers/<int:pk>/changelog/",
-        ObjectChangeLogView.as_view(),
+    path("receivers/", views.WebhookReceiverListView.as_view(),name="webhookreceiver_list",),
+    path("receivers/add/", views.WebhookReceiverEditView.as_view(),name="webhookreceiver_add",),
+    path("receivers/<int:pk>/", views.WebhookReceiverView.as_view(),name="webhookreceiver",),
+    path("receivers/<int:pk>/edit/", views.WebhookReceiverEditView.as_view(),name="webhookreceiver_edit",),
+    path("receivers/<int:pk>/delete/", views.WebhookReceiverDeleteView.as_view(),name="webhookreceiver_delete",),
+    path("receivers/<int:pk>/changelog/", ObjectChangeLogView.as_view(),
         name="webhookreceiver_changelog",
         kwargs={"model": models.WebhookReceiver},
     ),
