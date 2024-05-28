dae37e4f62b865158e01ec464e8beb6b833c809c
Lukasz Polanski <wookasz@gmail.com>
2024-05-28 17:46:45 +0200

    Switch to new Netbox user model.
    This is the first working version on Netbox v4.0

--- netbox_webhook_receiver/views/views.py
+++ netbox_webhook_receiver/views/views.py
@@ -2,7 +2,6 @@ import datetime as dt
 import json
 import logging
 
-from django.contrib.auth.models import User
 from django.db.transaction import atomic, non_atomic_requests
 from django.http import (
     HttpResponse,
@@ -14,6 +13,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from netbox_webhook_receiver.models import WebhookMessage, WebhookReceiver
 from secrets import compare_digest
+from users.models import User
 
 logger = logging.getLogger(__name__)
 
